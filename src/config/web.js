a3cf31524ba2684b8895b0c5d26d60bb80b767a5
al ex <fedotenko.aleksey@gmail.com>
2021-03-11 19:37:52 +0300

    Host change for prod

--- src/config/web.js
+++ src/config/web.js
@@ -1,4 +1,4 @@
-export const HOST = 'http://localhost:8080/';
+// export const HOST = 'http://localhost:8080/';
 
 
-// export const HOST = 'https://online-quiz-webservice.herokuapp.com/';
+export const HOST = 'https://online-quiz-webservice.herokuapp.com/';
